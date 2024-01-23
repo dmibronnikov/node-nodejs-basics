0f58f261bfc69607d7b961c1f936db91aec22a61
Dmitriy Bronnikov <dbron9269@gmail.com>
2024-01-23 04:23:16 +0200

    feat: implement fs/read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,12 @@
+import { promises as fs } from 'fs'
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const data = await fs.readFile('src/fs/files/fileToRead.txt', { encoding: 'utf8' });
+        console.log(data);
+    } catch {
+        throw 'FS operation failed';
+    }
 };
 
 await read();
