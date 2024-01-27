020b58e7f74c6f2cb50d064e88e062d00ce62d3a
Dmitriy Bronnikov <dbron9269@gmail.com>
2024-01-27 19:04:51 +0200

    fix: add \n to stdout writes

--- src/streams/read.js
+++ src/streams/read.js
@@ -2,10 +2,13 @@ import { createReadStream } from 'fs';
 import { pipeline } from 'stream/promises';
 
 const read = async () => {
-    await pipeline(
-        createReadStream('src/streams/files/fileToRead.txt'),
-        process.stdout
-    );
+    const fileReadStream = createReadStream('src/streams/files/fileToRead.txt');
+    fileReadStream.on('data', (chunk) => {
+        process.stdout.write(chunk.toString());
+    });
+    fileReadStream.on('end', () => {
+        process.stdout.write('\n');
+    });
 };
 
 await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,17 +1,24 @@
-import { Transform } from 'stream';
+import { Transform, Writable } from 'stream';
 import { pipeline } from 'stream/promises';
 
 const transform = async () => {
     let reverseTransform = new Transform({
-        transform(chunk, encoding, callback) {
-            callback(null, chunk.toString().split('').reverse().join(''));
+        transform(chunk, encoding, next) {
+            next(null, chunk.toString().split('').reverse().join(''));
         }
     });
+
+    let echo = new Writable({
+        write(chunk, encoding, next) {
+            process.stdout.write(`${chunk}\n`);
+            next();
+        }
+    })
 
     await pipeline(
         process.stdin,
         reverseTransform,
-        process.stdout
+        echo
     );
 };
 
