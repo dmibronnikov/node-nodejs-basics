685f191fa0049b9b50fd572c051918d7c47a11bb
Dmitriy Bronnikov <dbron9269@gmail.com>
2024-01-27 19:15:56 +0200

    feat: implement zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,13 @@
+import { pipeline } from 'stream/promises';
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+     await pipeline(
+        createReadStream('src/zip/files/filetoCompress.txt'),
+        createGzip(),
+        createWriteStream('src/zip/files/archive.gz')
+     );
 };
 
 await compress();
