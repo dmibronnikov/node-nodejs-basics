26ba381b7456db2ab616047b1460c0ffc21cf079
Dmitriy Bronnikov <dbron9269@gmail.com>
2024-01-27 22:57:27 +0200

    feat: implement cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,14 @@
+import { fork } from 'child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const path = `${import.meta.dirname}/files/script.js`;
+    const child = fork(path, args, { stdio: 'pipe' });
+
+    process.stdin.pipe(child.stdin);
+
+    child.stdout.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['argument1', 'argument2', 42]);
